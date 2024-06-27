a418100098f3aac054446a2884dfb99b54c04fee
manu@DESKTOP-FP8RVCH. <manu@DESKTOP-FP8RVCH.>
2024-06-27 01:34:58 +0000

    return fixed

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/', methods=["GET"])
 def entry():
     return """
-    <h1>Welcome to how create milanga</h1>
+    <h1>Welcome to how to create milanga</h1>
     """
            
